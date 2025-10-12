ec537732b5b69a79a94fe1d00fca20d514e797c8
Ollie-Edwards <79273130+Ollie-Edwards@users.noreply.github.com>
2025-10-12 15:39:45 +0100

    Redesign to agregate data into larger packets
    The polling rate was increased to 800hz, 10 datapoints are packed into a single UDP packet, this allows for a greater polling rate, while avoiding the overhead of sending UDP packets at a high frequency.
    
    Implemented circular buffer to read and write to queue in O(1) time and create a producer consumer design pattern, including a high frequency producer reading sensor data at 800hz and lower frequency consumer sending data to back end at 80hz.

--- main.ino
+++ main.ino
@@ -5,6 +5,8 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
+#include <CircularBuffer.hpp>
+
 Adafruit_MPU6050 mpu;
 
 // Create WiFi network
@@ -17,25 +19,116 @@ unsigned int localUdpPort = 4210; // UDP port
 char incomingPacket[255];
 char replyPacket[] = "Hello from ESP32 AP!";
 
-int sendDelay = 100;
-int status = 0;
+// Sample structure
+struct Sample {
+  uint32_t timestamp_ms;
+  float ax, ay, az, gx, gy, gz;
+};
+
 int counter = 0;
+
+// Setup circular buffer
+const int SAMPLE_RATE_HZ = 800;
+const int SEND_RATE_HZ = 80;
+const int BUFFER_SIZE = 1024;  // Must be > (SAMPLE_RATE_HZ / SEND_RATE_HZ)
+CircularBuffer<Sample, BUFFER_SIZE> sampleBuffer;
+
+unsigned long lastSampleMicros = 0;
+unsigned long lastSendMillis = 0;
+
+int status = 0; // stores whether or not the device is sending
 bool debug = false;
 
 const int buzzerPin = 33;
 
-void beep(int time)
-{
+void beep(int time) {
   digitalWrite(buzzerPin, HIGH);
   delay(time);
   digitalWrite(buzzerPin, LOW);
 }
 
-void setup()
-{
+void handleIncomingPacket() {
+  int packetSize = udp.parsePacket();
+  if (packetSize)
+  {
+    int len = udp.read(incomingPacket, 255);
+
+    // Add string terminator
+    if (len > 0)
+      incomingPacket[len] = 0;
+
+    if (debug){
+      Serial.printf("Received from %s:%d\n",
+        udp.remoteIP().toString().c_str(),
+        udp.remotePort());
+      Serial.printf("Message: %s\n", incomingPacket);
+    }
+
+    if (strcmp(incomingPacket, "Begin") == 0){
+      beep(500);
+      Serial.printf("Set status to 1");
+      status = 1;
+      counter = 0;
+    }
+
+    else if (strcmp(incomingPacket, "End") == 0){
+      beep(500);
+      Serial.printf("Set status to 0");
+      status = 0;
+      Serial.printf("send datapoints: %d\n", counter);
+
+    }
+  }
+}
+
+void sendMPUData(){
+  const int samplesPerPacket = SAMPLE_RATE_HZ / SEND_RATE_HZ;
+  Sample packet[samplesPerPacket];
+  int count = 0;
+
+  while (!sampleBuffer.isEmpty() && count < samplesPerPacket) {
+    packet[count++] = sampleBuffer.shift();
+  }
+
+  if (count > 0) {
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    udp.write((uint8_t*)packet, count * sizeof(Sample));
+    udp.endPacket();
+  }
+}
+
+// read MPU6050 sample and buffer
+void readMPUSensor() {
+  sensors_event_t a, g, temp_event;
+  mpu.getEvent(&a, &g, &temp_event);
+
+  counter  = counter + 1;
+
+  Sample s;
+  s.timestamp_ms = millis();
+  s.ax = a.acceleration.x;
+  s.ay = a.acceleration.y;
+  s.az = a.acceleration.z;
+  s.gx = g.gyro.x;
+  s.gy = g.gyro.y;
+  s.gz = g.gyro.z;
+
+  // Add to buffer
+  if (!sampleBuffer.isFull()) {
+    sampleBuffer.push(s);
+  } else {
+    sampleBuffer.shift();  // drop oldest
+    sampleBuffer.push(s);
+  }
+} 
+
+void setup() {
   pinMode(buzzerPin, OUTPUT);
 
   Serial.begin(115200);
+
+  Wire.begin(21, 22);
+  Wire.setClock(500000);
 
   if (!mpu.begin()) {
     Serial.println("MPU6050 not found!");
@@ -51,80 +144,29 @@ void setup()
   // Start UDP
   udp.begin(localUdpPort);
   Serial.printf("Listening on UDP port %d\n", localUdpPort);
+
+  beep(200);
+  delay(50);
+  beep(200);
+  delay(50);
+  beep(200);
 }
 
-void loop()
-{
-  counter ++;
-  // If packet recieved, read it and potentially change mode
-  int packetSize = udp.parsePacket();
-  if (packetSize)
-  {
-    int len = udp.read(incomingPacket, 255);
-    if (len > 0)
-      incomingPacket[len] = 0;
+void loop() {
+  unsigned long nowMicros = micros();
+  unsigned long nowMillis = millis();
 
-    if (debug){
-      Serial.printf("Received from %s:%d\n",
-        udp.remoteIP().toString().c_str(),
-        udp.remotePort());
-      Serial.printf("Message: %s\n", incomingPacket);
-    }
+  // If packet recieved, read it and potentially change status / mode
+  handleIncomingPacket();
 
-    if (strcmp(incomingPacket, "Begin") == 0){
-      beep(500);
-      Serial.printf("Set status to 1");
-      status = 1;
-    }
-
-    else if (strcmp(incomingPacket, "End") == 0){
-      beep(500);
-      Serial.printf("Set status to 0");
-      status = 0;
-    }
-
-    else{
-      Serial.printf("Set send delay");
-      sendDelay = atoi(incomingPacket);
-    }
+  if (nowMicros - lastSampleMicros >= 1000000UL / SAMPLE_RATE_HZ) {
+    lastSampleMicros = nowMicros;
+    readMPUSensor();
   }
 
-  if (status == 1){
-    // Send accelerometer data
-    Serial.println("send");
-
-    float data[7];
-    getAccelData(data);
-
-    udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.write((uint8_t*)data, 7 * sizeof(float));  // send as raw bytes
-    udp.endPacket();
-
-    if (counter % 10 == 0){
-      beep(1);
-    }
-
-    delay(sendDelay);
-  }
-
-  if (status == 0){
-    delay(10);
+  // Send data
+  if (status == 1 && (nowMillis - lastSendMillis >= 1000/SEND_RATE_HZ)) {
+    lastSendMillis = nowMillis;
+    sendMPUData();
   }
 }
-
-void getAccelData(float* data) {
-  sensors_event_t a, g, temp_event;
-  mpu.getEvent(&a, &g, &temp_event);
-
-  data[0] = millis();
-
-  data[1] = a.acceleration.x;
-  data[2] = a.acceleration.y;
-  data[3] = a.acceleration.z;
-
-  data[4] = g.gyro.x;
-  data[5] = g.gyro.y;
-  data[6] = g.gyro.z;
-
-  data[7] = temp_event.temperature;
-} 
